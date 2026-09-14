928de31f042b714116d65c133dd5b98c954126c3
Tanushrik <tanushrik311@gmail.com>
2026-09-14 21:53:11 +0530

    created first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <section id="center">
-   <h3>Hello World</h3>
-      </section>
-
-   
-      <section id="spacer"></section>
-    </>
-  )
+    <div>
+      <h1>My First Component</h1>
+    </div>
+  );
 }
-
-export default App
